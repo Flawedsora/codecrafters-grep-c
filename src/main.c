3bf4c0570664b57361557b2850101192efc0ac98
balaji <balajireset744@gmail.com>
2026-09-25 13:02:06 +0530

    added supprot for single-line file search [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,39 +8,6 @@
 
 #include "nfa.h"
 
-char **
-handleNewLine(const char *input_line)
-{
-	int   len  = (int)strlen(input_line);
-	char *dest = (char *)malloc(len + 2);
-	if (dest == NULL)
-		return NULL;
-	strcpy(dest, input_line);
-	dest[len]     = '\n';
-	dest[len + 1] = '\0';
-	char **fres   = (char **)calloc(100, sizeof(char *));
-	int    idx    = 0;
-	int    fidx   = 0;
-	while (idx < (int)strlen(dest)) {
-		int ilen = 0, cidx = idx;
-		while (cidx < (int)strlen(dest) && dest[cidx] != '\n')
-			cidx++, ilen++;
-		char *allocated = (char *)malloc(ilen + 1);
-		if (allocated == NULL)
-			return NULL;
-		int aidx = 0;
-		while (idx < (int)strlen(dest) && dest[idx] != '\n')
-			allocated[aidx++] = dest[idx++];
-		allocated[aidx] = '\0';
-		fres[fidx++]	= allocated;
-		if (dest[idx] == '\n')
-			idx++;
-	}
-	fres[fidx] = NULL;
-	free(dest);
-	return fres;
-}
-
 static void
 emit_only_match(const char *p, int plen, bool show_color)
 {
@@ -59,97 +26,87 @@ search_pattern(const char *input_buffer, const char *pattern,
 	t			   = insertConcats(t);
 	t			   = convertIntoPost(t);
 	struct NfaInfo nfa	   = thompson(t);
-	char	     **handle	   = handleNewLine(input_buffer);
 	// Hoisted anchor flags: same for every line/position.
 	bool	       startAnchor = (pattern[0] == '^');
 	bool	       endAnchor   = (strchr(pattern, '$') != NULL);
-	int	       i	   = 0;
-	bool	       final	   = false;
-	while (handle[i] != NULL) {
-		char  bufferAns[1024];
-		char *input = handle[i];
-		int   idx   = 0;
-		char *c	    = input;
-		bool  found = false;
-		if (startAnchor) {
-			int mlen = handleInput(c, &nfa, endAnchor);
-			if (mlen != -1) {
-				found = final = true;
-				if (only_matching) {
-					emit_only_match(c, mlen, showColory);
-					i++;
-					continue;
-				}
-				const char *red	      = "\033[1;31m";
-				const char *reset     = "\033[0m";
-				size_t	    red_len   = strlen(red);
-				size_t	    reset_len = strlen(reset);
-				if (showColory) {
-					memcpy(bufferAns + idx, red, red_len);
-					idx += (int)red_len;
-				}
-				memcpy(bufferAns + idx, c, (size_t)mlen);
-				idx += mlen, c += mlen;
-				if (showColory) {
-					memcpy(bufferAns + idx, reset,
-					    reset_len);
-					idx += (int)reset_len;
-				}
-				while (*c != '\0')
-					bufferAns[idx++] = *c, c++;
-				bufferAns[idx] = '\0';
-				printf("%s\n", bufferAns);
+	char	       bufferAns[1024];
+	int	       idx   = 0;
+	char	      *c     = input_buffer;
+	bool	       found = false;
+	if (startAnchor) {
+		int mlen = handleInput(c, &nfa, endAnchor);
+		if (mlen != -1) {
+			found = true;
+			if (only_matching) {
+				emit_only_match(c, mlen, showColory);
+				return true;
 			}
-			i++;
-			continue;
-		}
-		while (*c != '\0') {
-			int mlen = handleInput(c, &nfa, endAnchor);
-			if (mlen != -1) {
-				found = final = true;
-				if (only_matching) {
-					emit_only_match(c, mlen, showColory);
-					c += mlen;
-					if (mlen == 0 && *c != '\0')
-						c += 1;
-					continue;
-				}
-				if (mlen == 0) {
-					if (*c != '\0') {
-						bufferAns[idx++] = *c;
-						c += 1;
-					}
-					continue;
-				}
-				const char *red	      = "\033[1;31m";
-				const char *reset     = "\033[0m";
-				size_t	    red_len   = strlen(red);
-				size_t	    reset_len = strlen(reset);
-				if (showColory) {
-					memcpy(bufferAns + idx, red, red_len);
-					idx += (int)red_len;
-				}
-				memcpy(bufferAns + idx, c, (size_t)mlen);
-				idx += mlen, c += mlen;
-				if (showColory) {
-					memcpy(bufferAns + idx, reset,
-					    reset_len);
-					idx += (int)reset_len;
-				}
-			} else {
-				if (!only_matching)
-					bufferAns[idx++] = *c;
-				c += 1;
+			const char *red	      = "\033[1;31m";
+			const char *reset     = "\033[0m";
+			size_t	    red_len   = strlen(red);
+			size_t	    reset_len = strlen(reset);
+			if (showColory) {
+				memcpy(bufferAns + idx, red, red_len);
+				idx += (int)red_len;
 			}
-		}
-		if (!only_matching && found) {
+			memcpy(bufferAns + idx, c, (size_t)mlen);
+			idx += mlen, c += mlen;
+			if (showColory) {
+				memcpy(bufferAns + idx, reset, reset_len);
+				idx += (int)reset_len;
+			}
+			while (*c != '\0')
+				bufferAns[idx++] = *c, c++;
 			bufferAns[idx] = '\0';
 			printf("%s\n", bufferAns);
 		}
-		i++;
+		return found;
 	}
-	return final;
+	while (*c != '\0') {
+		int mlen = handleInput(c, &nfa, endAnchor);
+		if (mlen != -1) {
+			found = true;
+			if (only_matching) {
+				emit_only_match(c, mlen, showColory);
+				c += mlen;
+				if (mlen == 0 && *c != '\0')
+					c += 1;
+				continue;
+			}
+			if (mlen == 0) {
+				if (*c != '\0') {
+					bufferAns[idx++] = *c;
+					c += 1;
+				}
+				continue;
+			}
+			const char *red	      = "\033[1;31m";
+			const char *reset     = "\033[0m";
+			size_t	    red_len   = strlen(red);
+			size_t	    reset_len = strlen(reset);
+			if (showColory) {
+				memcpy(bufferAns + idx, red, red_len);
+				idx += (int)red_len;
+			}
+			memcpy(bufferAns + idx, c, (size_t)mlen);
+			idx += mlen, c += mlen;
+			if (showColory) {
+				memcpy(bufferAns + idx, reset, reset_len);
+				idx += (int)reset_len;
+			}
+		} else {
+			if (!only_matching)
+				bufferAns[idx++] = *c;
+			c += 1;
+		}
+	}
+	if (!only_matching && found) {
+		bufferAns[idx] = '\0';
+		printf("%s\n", bufferAns);
+	}
+	return found;
 }
+
 int
 main(int argc, char *argv[])
 {
@@ -203,18 +160,31 @@ main(int argc, char *argv[])
 		else if (isatty(1))
 			showColory = true;
 	}
-	char   input_buffer[4096];
-	int    c;
-	size_t total = 0;
-	while ((c = getchar()) != EOF) {
-		if (total + 1 < sizeof(input_buffer))
-			input_buffer[total++] = (char)c;
+	int   MAX_LEN = 4096;
+	char  input_buffer[MAX_LEN];
+	void *input_taker;
+	if (optind < argc) {
+		const char *filename = argv[optind];
+		FILE	   *file     = fopen(filename, "r");
+		if (file == NULL) {
+			fprintf(stderr, "CANNOT OPEN FILE.\n");
+			return 1;
+		}
+		input_taker = file;
+	} else
+		input_taker = stdin;
+	bool returnVal = false;
+	// no filename read stdin
+	while (fgets(input_buffer, MAX_LEN, input_taker)) {
+		input_buffer[strcspn(input_buffer, "\n")] = 0;
+		// pass this buffer directly to search_pattern fxn
+		fprintf(stderr, "Value of input buffer is: %s", input_buffer);
+		if (search_pattern(input_buffer, pattern, only_matching,
+			showColory))
+			returnVal = true;
 	}
-	if (total == 0)
-		return 1;
-	input_buffer[total] = '\0';
 	// Remove trailing newline
-	if (search_pattern(input_buffer, pattern, only_matching, showColory)) {
+	if (returnVal) {
 		return 0;
 	} else {
 		return 1;
